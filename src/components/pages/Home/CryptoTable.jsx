5c013fdbb0e2b9f358320b3ad492bde3f9f6f791
mouad-p7 <mouadananouch7@gmail.com>
2023-08-02 22:33:25 +0100

    Get Cryptocurrency data from binance api.

--- src/components/pages/Home/CryptoTable.jsx
+++ src/components/pages/Home/CryptoTable.jsx
@@ -1,33 +1,26 @@
 import { useEffect, useState } from 'react';
-import { fetchCryptoData } from '../../../utils/api';
+import { fetchData } from '../../../utils/api';
+import Pct from '../../common/Pct';
 import './styles/CryptoTable.css';
 
 
 export default function CryptoTable() {
-  const [cryptoData, setCryptoData] = useState([
-    {
-      id: '1',  
-      name: 'Bitcoin',
-      symbol: 'BTC', 
-      price: '29,522', 
-      percentChange24h: '2.39',
-      marketCap: '555,555,555,555', 
-      volume: '22,222,222,222', 
-    },
-    {
-      id: '2',  
-      name: 'Ethereum',
-      symbol: 'ETH', 
-      price: '1,850', 
-      percentChange24h: '1.51',
-      marketCap: '222,222,222,222', 
-      volume: '7,777,777,777', 
+  const [cryptoData, setCryptoData] = useState([]);
+  const [fallBack, setFallBack] = useState('');
+  const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+
+
+  async function fetchLatest() {
+    try {
+      setCryptoData(await fetchData(apiUrl));
+    } catch (error) {
+      setFallBack(error);
     }
-  ]);
+  };
 
 
   useEffect(() => {
-    // fetchCryptoData();
+    fetchLatest();
   }, []);
 
 
@@ -40,28 +33,54 @@ export default function CryptoTable() {
             <th>#</th>
             <th>Name</th>
             <th>Price</th>
+            <th>1h %</th>
+            <th>24h %</th>
+            <th>7d %</th>
             <th>Market Cap</th>
-            <th>24h Volume</th>
-            <th>% Change (24h)</th>
+            <th>Volume (24h)</th>
+            <th>Circulating Supply</th>
             <th>Price Graph</th>
           </tr>
         </thead>
         <tbody>
           {
-            cryptoData.map((crypto, index) => (
-              <tr key={crypto.id}>
-                <td>⭐</td>
-                <td>{index}</td>
-                <td>{crypto.name} ({crypto.symbol})</td>
-                <td>{crypto.price}</td>
-                <td>{crypto.percentChange24h}</td>
-                <td>{crypto.marketCap}</td>
-                <td>{crypto.volume}</td>
-                <td>
-                  {/* The price graph */}
-                </td>
-              </tr>
-            ))
+            cryptoData && cryptoData !== []
+            ? cryptoData.map((crypto, index) => (
+                <tr key={crypto.id}>
+                  <td>
+                    ⭐
+                  </td>
+                  <td>{index + 1}</td>
+                  <td>
+                    {crypto.name} {crypto.symbol}
+                  </td>
+                  <td>
+                    $ {crypto.quote.USD.price}
+                  </td>
+                  <td>
+                    <Pct pct={crypto.quote.USD.percent_change_1h}/>
+                  </td>
+                  <td>
+                    <Pct pct={crypto.quote.USD.percent_change_24h}/>
+                  </td>
+                  <td>
+                    <Pct pct={crypto.quote.USD.percent_change_7d}/>
+                  </td>
+                  <td>
+                    $ {crypto.quote.USD.market_cap}
+                  </td>
+                  <td>
+                    $ {crypto.quote.USD.volume_24h}
+                  </td>
+                  <td>
+                    {crypto.circulating_supply}
+                  </td>
+                  <td>
+                    {/* The price graph */}
+                  </td>
+                </tr>
+              )) 
+            : <p>{fallBack ? fallBack : 'loading...'}</p>
           }
         </tbody>
       </table>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,17 @@
-export async function fetchCryptoData(apiUrl){
+const apiKey = '75288f73-da84-46f4-8300-e1e3da779244';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+
+export async function fetchData(apiUrl){
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    const response = await fetch(proxyUrl + apiUrl, {
+      headers: {'X-CMC_PRO_API_KEY': apiKey}
+    });
+    if (!response.ok) throw new Error('Network response was not ok');
+    const jsonData = await response.json()
+    return jsonData.data;
   } catch(error) {
-    console.error('Error fetching data: ', error);
+    throw new Error(`Error fetching data: ${error.message}`);
   }
 };
+
