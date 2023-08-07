7421505ab1b6d3810766bbee5b1e3d85e8e52fd0
mouad-p7 <mouadananouch7@gmail.com>
2023-08-07 14:13:25 +0100

    Formate data using Intl.NumberFormat api.

--- src/components/pages/Home/columns.js
+++ src/components/pages/Home/columns.js
@@ -1,11 +1,29 @@
+const priceFormatter = new Intl.NumberFormat('en-us', { 
+  style: 'currency', currency: 'USD', minimumFractionDigits: 9
+});
+const compactPriceFormatter = new Intl.NumberFormat('en-us', { 
+  style: 'currency', currency: 'USD', notation: 'compact' 
+});
+const compactNumFormatter = new Intl.NumberFormat('en-us', { 
+  notation: 'compact' 
+});
+
+
+function formatPrice(value){
+  if (Math.abs(value) < 0.001) return priceFormatter.format(value);
+  else return `$${value.toFixed(2)}`;
+};
+
+
 export default [
+  {id: '', label: ''},
   {id: 'cmc_rank', label: '#'},
-  {id: 'name', label: 'Name', minWidth: 150},
+  {id: 'name', label: 'Name', minWidth: 170},
   {
     id: 'price', 
     label: 'Price', 
     align: 'center',
-    format: (value) => `$${value.toFixed(2)}`
+    format: (value) => formatPrice(value)
   },
   {
     id: 'percent_change_1h', 
@@ -30,20 +48,23 @@ export default [
   },
   {
     id: 'market_cap', 
-    label: 'Market Cap', 
+    label: 'Market Cap',
+    minWidth: 125,  
     align: 'center',
-    format: (value) => `$${value.toLocaleString('en-US')}`
+    format: (value) => compactPriceFormatter.format(value)
   },
   {
     id: 'volume_24h', 
     label: 'Volume (24h)', 
+    minWidth: 125, 
     align: 'center', 
-    format: (value) => `$${value.toLocaleString('en-US')}`
+    format: (value) => compactPriceFormatter.format(value)
   },
   {
     id: 'circulating_supply',
-    label: 'Circulating Supply', 
+    label: 'Circulating Supply',
+    minWidth: 155,  
     align: 'center',
-    format: (value) => `$${value.toLocaleString('en-US')}`
+    format: (value) => compactNumFormatter.format(value)
   }
 ];
