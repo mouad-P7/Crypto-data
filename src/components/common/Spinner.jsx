117a4ed81f00044a0a2d001a8c9c4f3a24cf5397
mouad-p7 <mouadananouch7@gmail.com>
2023-08-11 20:35:56 +0100

    Change loading <Spinner/> animation.

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -3,15 +3,8 @@ import './styles/Spinner.css';
 
 export default function Spinner({size = '20px', ...restProps}) {
   return (
-    <div className="dot-spinner" style={{width: size, height: size }} {...restProps}>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-      <div className="dot-spinner__dot"></div>
-    </div>
+    <svg className='spinner' viewBox="25 25 50 50" style={{width: size }} {...restProps}>
+      <circle r="20" cy="50" cx="50"></circle>
+    </svg>
   );
 };
