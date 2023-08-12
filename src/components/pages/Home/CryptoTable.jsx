90996286b4ff86c23c86315a79799ff867937c2f
mouad-p7 <mouadananouch7@gmail.com>
2023-08-12 21:20:00 +0100

    Handle <CryptoTable/> api errors.

--- src/components/pages/Home/CryptoTable.jsx
+++ src/components/pages/Home/CryptoTable.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Alert from '@mui/material/Alert';
 import { fetchData } from '../../../utils/api';
 import Spinner from '../../common/Spinner'
 import CoinsTable from './CoinsTable';
@@ -7,6 +8,7 @@ import CoinsTable from './CoinsTable';
 export default function CryptoTable() {
   const [cryptoData, setCryptoData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
   const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
 
 
@@ -15,6 +17,7 @@ export default function CryptoTable() {
       setCryptoData(await fetchData(apiUrl));
     } catch (error) {
       console.error(error);
+      setError(error.message);
     } finally {
       setLoading(false);
     }
@@ -30,6 +33,11 @@ export default function CryptoTable() {
         <Spinner size='60px' />
       </div>
     );
+  }
+
+
+  if(error !== ''){
+    return <Alert severity="error">{error}</Alert>;
   }
 
 
