d475816b3215070fbe5f897e6be88d210d5f1137
mouad-p7 <mouadananouch7@gmail.com>
2023-08-23 15:27:38 +0100

    Create the price chart demo in <Chart/> using react-chartjs-2.

--- src/components/pages/Home/CoinDetails/Chart.jsx
+++ src/components/pages/Home/CoinDetails/Chart.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 import Alert from '@mui/material/Alert';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import { fetchData } from '../../../../utils/api';
 import Spinner from '../../../common/Spinner';
 
 
+ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+
 export default function Chart({slug}) {
   const [coinMarketData, setCoinMarketData] = useState([]);
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
-  const apiUrl = ``;
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/${slug}/market_chart?vs_currency=usd&days=1`;
 
 
   async function fetchCoinMarketData() {
     try {
-      setCoinMarketData(await fetchData(apiUrl));
+      setCoinMarketData(await fetchData(apiUrl, 'prices', false));
     } catch (error) {
       console.error(error);
       setError(error.message);
@@ -23,16 +28,16 @@ export default function Chart({slug}) {
   };
 
 
-  // useEffect(() => {fetchCoinMarketData()}, []);
+  useEffect(() => {fetchCoinMarketData()}, []);
 
 
-  // if(loading){
-  //   return (
-  //     <div className="spinner-ctr" style={{ marginTop: '60px' }}>
-  //       <Spinner size='60px' />
-  //     </div>
-  //   );
-  // }
+  if(loading){
+    return (
+      <div className="spinner-ctr" style={{ marginTop: '60px' }}>
+        <Spinner size='60px' />
+      </div>
+    );
+  }
 
 
   if(error !== ''){
@@ -41,8 +46,22 @@ export default function Chart({slug}) {
 
 
   return (
-    <div>
-      Chart
-    </div>
+    <Line
+      data={{
+        labels: coinMarketData.map(item => item[0]),
+        datasets: [
+          {
+            data: coinMarketData.map(item => item[1])
+          }
+        ]
+      }}
+      options={{
+        responsive: true,
+        plugins: {
+          legend: { display: false },
+          title: { display: true, text: `${slug} to USD Chart` }
+        }
+      }}
+    />
   );
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,14 +2,21 @@ const apiKey = '75288f73-da84-46f4-8300-e1e3da779244';
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 
-export async function fetchData(apiUrl){
+export async function fetchData(apiUrl, key = 'data', proxy = true){
   try {
-    const response = await fetch(proxyUrl + apiUrl, {
-      headers: {'X-CMC_PRO_API_KEY': apiKey}
-    });
+    let response;
+    if(proxy){
+      response = await fetch(proxyUrl + apiUrl, {
+        headers: {'X-CMC_PRO_API_KEY': apiKey}
+      });
+    }else{
+      response = await fetch(apiUrl, {
+        headers: {'X-CMC_PRO_API_KEY': apiKey}
+      });
+    }
     if (!response.ok) throw new Error('Network response was not ok');
     const jsonData = await response.json();
-    return jsonData.data;
+    return jsonData[key];
   } catch(error) {
     throw new Error(`Error fetching data: ${error.message}`);
   }
