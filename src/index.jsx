3dd82dd493f8d308ee3b52347516fd2dfcb6fbc1
mouad-p7 <mouadananouch7@gmail.com>
2023-08-27 16:11:19 +0100

    Add cryptoData redux state.

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,15 @@ import App from './App.jsx';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import mobileMenuReducer from './redux/mobileMenu';
+import cryptoDataReducer from './redux/cryptoData.js';
 import './variables.css';
 import './global.css';
 
 
 const store = configureStore({
   reducer: {
-    mobileMenu: mobileMenuReducer
+    mobileMenu: mobileMenuReducer,
+    cryptoData: cryptoDataReducer,
   }
 });
 
